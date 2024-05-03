5f8b936351d8d761dc5e4e417cf08c63c5d9b3f0
Sandeep Lal <sandeep221102@gmail.com>
2024-05-03 18:47:36 +0530

    add-recipe path added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AllRecipeComponent } from './components/all-recipe/all-recipe.component';
+import { InputFormComponent } from './components/input-form/input-form.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/home', pathMatch:'full'},
   {path: 'home', component:HomeComponent},
-  {path: 'all-recipe', component:AllRecipeComponent}
+  {path: 'all-recipe', component:AllRecipeComponent},
+  {path: 'add-recipe', component:InputFormComponent}
 ];
 
 @NgModule({
